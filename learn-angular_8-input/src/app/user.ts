fb3a5673da316f96bc81dc4eff84c85686f1bc27
ppfuark <phfuark2007@gmail.com>
2025-07-29 16:48:17 -0300

    Class[feat]: 5, 6, 7, 8, 9

---
+++ learn-angular_8-input/src/app/user.ts
@@ -0,0 +1,11 @@
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>The user's occupation is {{occupation()}}</p>
+  `,
+})
+export class User {
+  occupation = input<string>();
+}
