21a12c10d9d202ae3d7b9973d0c4d388d9c58e13
ppfuark <ppfuark2007@gmail.com>
2025-07-29 16:45:22 -0300

    Class[feat]: 5, 6, 7, 8, 9

--- learn-angular_8-input/src/app/app.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-import {User} from './user';
-
-@Component({
-  selector: 'app-root',
-  template: `<app-user occupation="Angular Developer"></app-user>`,
-  imports: [User],
-})
-export class App {}
